82a28a8b634782d68153256c4593bb5a5623037c
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-10-16 20:58:26 +0900

    Title: 두줄 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241016/두줄 출력/print-two-sentences-introduce.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //cout << "Hello students!\nWelcome to LeebrosCode!";
+    cout << "Hello students!";
+    cout << endl;
+    cout << "Welcome to LeebrosCode!";
+    return 0;
+}
