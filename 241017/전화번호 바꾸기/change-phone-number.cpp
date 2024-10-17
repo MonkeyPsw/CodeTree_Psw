73da92ff32c6be6771dce8c236b547c7f58ceec1
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-10-17 22:11:57 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241017/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int base, a, b;
+    cin >> base;
+    cin.get();
+    cin >> a;
+    cin.get();
+    cin >> b;
+    cout << "010-" << b << "-" << a;
+    return 0;
+}
