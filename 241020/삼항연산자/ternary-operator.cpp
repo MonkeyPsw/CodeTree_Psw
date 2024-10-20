5c31f6924c4752e7096a0430d17fab2f911ba233
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-10-20 18:56:52 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241020/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int score;
+    string grade;
+
+    cin >> score;
+
+    grade = score == 100 ? "pass" : "failure";
+
+    cout << grade;
+
+    return 0;
+}
