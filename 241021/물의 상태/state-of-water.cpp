b6e056e3e44399a5dbeffe184cb2edbf40a94bc8
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-10-21 23:14:52 +0900

    Title: 물의 상태, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241021/물의 상태/state-of-water.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    
+    cin >> n;
+
+    if (n >= 100)
+        cout << "vapor";
+    else if (n < 0)
+        cout << "ice";
+    else
+        cout << "water";
+
+    return 0;
+}
