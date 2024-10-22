c05cff667dddce2ba1bf2a37a0e0ad9c0d6e0224
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-10-22 17:24:51 +0900

    Title: 점수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241022/점수 비교/score-comparison.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int aMath, aEng;
+    int bMath, bEng;
+
+    cin >> aMath >> aEng;
+    cin >> bMath >> bEng;
+
+    cout << (aMath > bMath && aEng > bEng);
+
+    return 0;
+}
