f7028da17126cde851e1d91c10b8b7349346b470
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-10-23 20:49:29 +0900

    Title: 숫자의 계절은, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241023/숫자의 계절은/season-of-num.cpp
+++ 241023/숫자의 계절은/season-of-num.cpp
@@ -12,5 +12,15 @@ int main() {
         cout << "Fall";
     else
         cout << "Winter";
+    /*
+    if(m >= 12 || m <= 2)
+		cout << "Winter";
+	else if(m <= 5)
+		cout << "Spring";
+	else if(m <= 8)
+		cout << "Summer";
+	else
+		cout << "Fall";
+    */
     return 0;
 }
