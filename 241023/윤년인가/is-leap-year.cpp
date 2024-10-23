4192414e1724ab9e5a92d07459d61ac6fe6ba34f
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-10-23 21:22:59 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241023/윤년인가/is-leap-year.cpp
+++ 241023/윤년인가/is-leap-year.cpp
@@ -4,9 +4,30 @@ using namespace std;
 int main() {
     int y;
     cin >> y;
+    // y % 4 == 0 && y % 100 != 0) || y % 400 == 0 true 이게 더 낫네
     if (y % 4 != 0 || y % 100 == 0 && y % 400 != 0)
         cout << "false";
     else
         cout << "true";
+    /*
+    굳이 이렇게?
+    if(y % 4 == 0) {
+		if(y % 100 == 0) {
+			if(y % 400 == 0) {
+				cout << "true";
+			}
+			else {
+				cout << "false";
+			}
+		}
+		else {
+			cout << "true";
+		}
+	}
+	else {
+		cout << "false";
+	}
+    */
+
     return 0;
 }
