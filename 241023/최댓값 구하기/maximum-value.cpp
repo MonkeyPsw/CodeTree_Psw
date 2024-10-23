b37248a69d6dc3158885a52dd8964f39b1f3c77a
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-10-23 21:37:22 +0900

    Title: 최댓값 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241023/최댓값 구하기/maximum-value.cpp
+++ 241023/최댓값 구하기/maximum-value.cpp
@@ -11,5 +11,24 @@ int main() {
         cout << b;
     else
         cout << c;
+    
+    /* 중첩 조건문
+    // a와 b를 비교한뒤, a가 b보다 크다면 a와 c를 비교하여 최댓값을 구합니다.
+    if(a >= b) {
+		if(a >= c)
+			cout << a;
+		else
+			cout << c; 
+	}		
+
+	// a와 b를 비교한 결과가 나와있으므로, b와 c만 비교하여 최댓값을 구합니다.
+	else {
+		if(b >= c)
+			cout << b;
+		else
+			cout << c;
+	}
+    */
+
     return 0;
 }
