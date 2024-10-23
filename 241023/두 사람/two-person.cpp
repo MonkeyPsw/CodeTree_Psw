920e401e23348f99a7acb9a40eb020fd4e6c4cb0
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-10-23 21:07:24 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241023/두 사람/two-person.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int aAge, bAge;
+    char aSex, bSex;
+
+    cin >> aAge >> aSex;
+    cin >> bAge >> bSex;
+
+    cout << (aAge >= 19 && aSex == 'M' || bAge >= 19 && aSex == 'M');
+
+    return 0;
+}
