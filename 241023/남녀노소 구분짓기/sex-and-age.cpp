0ab358dc6f436412264b631fa7ee36616486308c
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-10-23 21:17:06 +0900

    Title: 남녀노소 구분짓기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241023/남녀노소 구분짓기/sex-and-age.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int gen, age;
+    cin >> gen >> age;
+    if (gen)
+    {
+        if (age >= 19)
+            cout << "WOMAN";
+        else
+            cout << "GIRL";
+    }
+    else
+    {
+        if (age >= 19)
+            cout << "MAN";
+        else
+            cout << "BOY";
+    }
+    return 0;
+}
