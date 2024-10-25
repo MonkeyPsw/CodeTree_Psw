7761724ef9b57570ddcda71682935618d8fe838a
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-10-25 22:21:09 +0900

    Title: a/b 출력, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241025/a/b 출력/a-divide-b.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cin >> a >> b;
+
+    cout << a / b << ".";
+    for (int i = 0; i < 20; i++)
+    {
+        cout << a * 10 / b;
+        a = a * 10 - b * (a * 10 / b);
+    }
+
+    return 0;
+}
