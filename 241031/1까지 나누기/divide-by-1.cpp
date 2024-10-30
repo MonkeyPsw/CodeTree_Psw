881c7e7e7ac0a3b89671c8871bcf63b72bcc136e
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-10-31 00:37:12 +0900

    Title: 1까지 나누기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 241031/1까지 나누기/divide-by-1.cpp
+++ 241031/1까지 나누기/divide-by-1.cpp
@@ -10,9 +10,19 @@ int main() {
         if (n <= 1)
         {
             cout << i;
-            break;
+            break; // 생각해보니 while에는 break 어차피 없어도 되네
         }
         i++;
     }
+    /*
+    for(int i = 1; i <= 100; i++) {
+        if(n <= 1) {
+            cout << cnt;
+            break;
+        }
+        n /= i;
+        cnt++;
+    }
+    */
     return 0;
 }
