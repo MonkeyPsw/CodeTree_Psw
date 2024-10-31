8fed4afbca86f59ee8e03ac7ad7da3af3d5c2626
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-10-31 19:43:03 +0900

    Title: ab 사이에 없는 c, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241031/ab 사이에 없는 c/c-between-a-and-b-2.cpp
+++ 241031/ab 사이에 없는 c/c-between-a-and-b-2.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 int main() {
     int a, b, c;
-    bool isC = true;
+    bool isNotC = true;
     cin >> a >> b >> c;
     for (int i = a; i <= b; i++)
     {
-        if (i % c != 0)
-            isC = false;
+        if (i % c == 0)
+            isNotC = false;
     }
-    if (!isC)
+    if (isNotC)
         cout << "YES";
     else
         cout << "NO";
