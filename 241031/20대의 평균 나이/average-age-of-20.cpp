b00f2a7bd8c179cdc9afd1fc0d83810fff6c2082
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-10-31 18:47:22 +0900

    Title: 20대의 평균 나이, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241031/20대의 평균 나이/average-age-of-20.cpp
+++ 241031/20대의 평균 나이/average-age-of-20.cpp
@@ -20,6 +20,24 @@ int main() {
             break;
         }
     }
+    /* else를 안쓰는게 좋나?
+    while(1) {
+        // 변수를 선언하고 입력을 받습니다.
+        int n;
+        cin >> n;
+        
+        // 입력받은 값이 20대가 아니면 종료합니다.
+        if(n >= 30 || n <= 19) {
+            cout << fixed;
+            cout.precision(2);
+            cout << (double)sum / cnt;
+            break;
+        }
 
+        // 20대가 맞는 경우에는, 계속 값을 계산해줍니다.
+        sum += n;
+        cnt++;
+    }
+    */
     return 0;
 }
