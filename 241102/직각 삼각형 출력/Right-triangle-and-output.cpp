f58157f1c1fd98faafdff24fe9ed4614ea9f5d3f
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-11-02 00:25:27 +0900

    Title: 직각 삼각형 출력, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 241102/직각 삼각형 출력/Right-triangle-and-output.cpp
+++ 241102/직각 삼각형 출력/Right-triangle-and-output.cpp
@@ -5,9 +5,9 @@ int main() {
     int n;
     cin >> n;
 
-    for (int i = 1; i <= n; i++)
+    for (int i = 1; i <= n; i++) // (int i = 0; i < n; i++)
     {
-        for (int j = 1; j <= (i * 2 - 1); j++)
+        for (int j = 1; j <= (i * 2 - 1); j++) // (int j = 0; j < 2 * i + 1; j++)
             cout << "*";
         cout << endl;
     }
