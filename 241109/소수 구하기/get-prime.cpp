ffd7ac95c6e674644328de2db535721dfdbd294e
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-11-09 20:46:28 +0900

    Title: 소수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241109/소수 구하기/get-prime.cpp
+++ 241109/소수 구하기/get-prime.cpp
@@ -15,5 +15,18 @@ int main() {
         if (divCnt == 2)
             cout << i << " ";
     }
+    /*
+    // 1부터 n까지 소수를 구합니다.
+	for(int i = 1; i <= n; i++) {
+		if(i == 1) continue;
+		bool isprime = true;
+		
+		for(int j = 2; j < i; j++)
+			if(i % j == 0) isprime = false;
+		
+		if(isprime)
+			cout << i << " ";
+	}
+    */
     return 0;
 }
