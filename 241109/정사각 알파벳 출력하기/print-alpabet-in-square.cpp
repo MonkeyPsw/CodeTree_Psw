7dc80b17334831bb83bfbd3002afa187c181458f
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-11-09 20:10:32 +0900

    Title: 정사각 알파벳 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241109/정사각 알파벳 출력하기/print-alpabet-in-square.cpp
+++ 241109/정사각 알파벳 출력하기/print-alpabet-in-square.cpp
@@ -10,5 +10,17 @@ int main() {
             cout << (char)cnt++;
         cout << endl;
     }
+    /*
+    char cnt = 'A';
+	
+	// 알파벳을 정사각형 모양으로 출력합니다.
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < n; j++) {
+			cout << cnt;
+			cnt++;
+		}
+		cout << endl;
+	}
+    */
     return 0;
 }
