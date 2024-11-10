44601dd87c605cf3d72009bd16e19f7e230e7638
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-11-10 19:01:39 +0900

    Title: 원소 10개의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241110/원소 10개의 합/sum-of-10-elements.cpp
+++ 241110/원소 10개의 합/sum-of-10-elements.cpp
@@ -8,6 +8,25 @@ int main() {
 		sum += num;
 	}
 	cout << sum;
+    /* 1차원 배열 파트라서 일단은
+    // 크기가 10인 배열을 구현합니다.
+    int arr[10];
+    int sum = 0;
 
+	// 0부터 9까지의 인덱스에 주어진 수를 차례대로 입력받습니다.
+    for(int i = 0; i < 10; i++) {
+        cin >> arr[i];
+    }
+	
+	// 입력받은 수들을  sum에 더합니다.
+	for(int i = 0; i < 10; i++) {
+        sum += arr[i];
+    }
+	
+    // sum을 출력합니다.
+    cout << sum;
+
+    return 0;
+    */
 	return 0;
 }
