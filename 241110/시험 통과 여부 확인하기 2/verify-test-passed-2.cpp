a6965c7a875ba189b83cecde63eadf7256fc3211
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-11-10 23:40:58 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241110/시험 통과 여부 확인하기 2/verify-test-passed-2.cpp
+++ 241110/시험 통과 여부 확인하기 2/verify-test-passed-2.cpp
@@ -4,7 +4,10 @@ using namespace std;
 int main() {
     int n, cnt = 0;
     int Score[4];
-    int Sum[10] = {};
+    int Sum[10] = {}; // sum은 굳이 배열 안해도 되는거였다.
+    // 교실에는 10명보다 적은 학생들이 있습니다.
+    // 첫 번째 줄에 10명 미만의 학생수가 주어집니다.
+    // 라면서 두번째 테스트 케이스가 10인데 뭐냐 이거.
 
     cin >> n;
     for (int i = 0; i < n; i++)
