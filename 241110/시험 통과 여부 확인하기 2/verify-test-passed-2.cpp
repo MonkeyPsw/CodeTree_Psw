4dd440f9bb1c55ea2fb95288627886fadd07245a
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-11-10 23:32:28 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241110/시험 통과 여부 확인하기 2/verify-test-passed-2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, cnt = 0;
+    int Score[4];
+    int Sum[9] = {};
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cin >> Score[j];
+            Sum[i] += Score[j];
+        }
+        if ((double)Sum[i] / 4 >= 60)
+        {
+            cout << "pass";
+            cnt++;
+        }
+        else
+            cout << "fail";
+        cout << endl;
+    }
+    cout << cnt;
+    
+    return 0;
+}
