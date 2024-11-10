b2243db1e772299410dc5e86ec2414372807f0ce
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-11-10 20:22:14 +0900

    Title: 배열 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241110/배열 채우기/filling-array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int cnt = 0;
+    int arr[10];
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> arr[i];
+        if (arr[i] == 0)
+            break;
+        cnt++;
+    }
+    for (int i = cnt - 1; i >= 0; i--)
+        cout << arr[i] << " ";
+    return 0;
+}
