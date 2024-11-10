eb7bf81dff111a55768edbb13abb7c8e5f826403
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-11-10 19:16:27 +0900

    Title: 특정 숫자 도달하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241110/특정 숫자 도달하기/reaching-specific-number.cpp
+++ 241110/특정 숫자 도달하기/reaching-specific-number.cpp
@@ -3,19 +3,18 @@ using namespace std;
 
 int main() {
     int arr[10];
-    int num, sum = 0;
-    for (int i = 0; i <= 9; i++)
+    int num, sum = 0, cnt = 0;
+    for (int i = 0; i < 10; i++)
     {
         cin >> arr[i];
         if (arr[i] < 250)
             sum += arr[i];
         else
-        {
-            cout << fixed;
-            cout.precision(1);
-            cout << sum << " " << (double)sum / i;
             break;
-        }
+        cnt++;
     }
+    cout << fixed;
+    cout.precision(1);
+    cout << sum << " " << (double)sum / cnt;
     return 0;
 }
