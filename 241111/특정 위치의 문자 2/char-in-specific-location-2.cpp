3f99eace623aa2be549324120fd2368620551dd8
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-11-11 23:35:38 +0900

    Title: 특정 위치의 문자 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241111/특정 위치의 문자 2/char-in-specific-location-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char arr[10];
+    for (int i = 0; i < 10; i++)
+        cin >> arr[i];
+    cout << arr[1] << " " << arr[4] << " " << arr[7];
+    return 0;
+}
+// 채점이 거의 2시간동안 안되는건 너무했다.
+// 깃허브 연동도 자꾸 풀리네.
