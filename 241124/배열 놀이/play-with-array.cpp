f463ace634e910baedd73b2b4e190b2a3dff39f2
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-11-24 21:02:16 +0900

    Title: 배열 놀이, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241124/배열 놀이/play-with-array.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, q;
+	int quest, a, b, s, e;
+	int idx = -1;
+    int arr[100];
+	cin >> n >> q;
+
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+	
+	for (int i = 0; i < q; i++)
+	{
+		cin >> quest;
+		switch (quest)
+		{
+			case 1:
+				cin >> a;
+				cout << arr[a - 1];
+				break;
+			case 2:
+				cin >> b;
+				for (int i = 0; i < n; i++)
+				{
+					if (arr[i] == b)
+					{
+						idx = i;
+						break;
+					}
+				}
+				if (idx == -1)
+					cout << 0;
+				else
+					cout << ++idx;
+				break;
+			case 3:
+				cin >> s >> e;
+				for (int i = s - 1; i < e; i++)
+					cout << arr[i] << " ";
+				break;
+		}
+		cout << endl;
+	}
+    return 0;
+}
