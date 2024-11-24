1357b188395f8fb222d510ce200c1b367ff04df1
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-11-24 20:29:08 +0900

    Title: 나눗셈의 나머지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241124/나눗셈의 나머지/remainder-of-division.cpp
+++ 241124/나눗셈의 나머지/remainder-of-division.cpp
@@ -8,6 +8,19 @@ int main() {
 
     cin >> a >> b;
 
+	/* 나머지를 바로 저장할수도 있구나
+	// a가 1 이하가 될 때 까지 나누면서 나머지를 count배열에 저장하기
+	while(a > 1) {
+        count_arr[a % b]++;
+        a /= b;
+    }
+	
+    // 나머지가 나온 횟수를 구해 정답 구하기
+    for(int i = 0; i < b; i++) {
+        ans += count_arr[i] * count_arr[i];
+    }
+	*/
+
     while (a > 1)
     {
         arr[cnt++] = a % b;
