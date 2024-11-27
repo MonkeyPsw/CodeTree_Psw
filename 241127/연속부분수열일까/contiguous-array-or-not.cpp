1a8affd8e8fb47759159438a1de6beee3747bdc9
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-11-27 17:49:59 +0900

    Title: 연속부분수열일까, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241127/연속부분수열일까/contiguous-array-or-not.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	bool isSub = true;
+    int n1, n2;
+	int a[100], b[100];
+	cin >> n1 >> n2;
+
+	for (int i = 0; i < n1; i++)
+		cin >> a[i];
+	for (int i = 0; i < n2; i++)
+		cin >> b[i];
+
+	for (int i = 0; i < n1; i++)
+	{
+		if (!isSub)
+			break;
+			
+		if (a[i] == b[0])
+		{
+			if (i + n2 - 1 <= n1)
+			{
+				for (int j = 0; j < n2; j++)
+				{
+					if (a[i + j] != b[j])
+					{
+						isSub = false;
+						break;
+					}
+				}
+			}
+		}
+		else
+			isSub = false;
+	}
+
+	cout << (isSub ? "Yes" : "No");
+
+    return 0;
+}
