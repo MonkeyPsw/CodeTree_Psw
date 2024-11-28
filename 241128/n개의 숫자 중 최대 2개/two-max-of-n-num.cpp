bfd73116fa2f9aadd7112ca41399d4014ce1bb79
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-11-28 23:02:53 +0900

    Title: n개의 숫자 중 최대 2개, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 241128/n개의 숫자 중 최대 2개/two-max-of-n-num.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int arr[100];
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        int max = arr[i];
+        int idx;
+
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] >= max)
+            {
+                max = arr[j];
+                idx = j;
+            }
+        }
+
+        int temp = arr[i];
+        arr[i] = max;
+        arr[idx] = temp;
+    }
+
+    cout << arr[0] << " " << arr[1];
+    return 0;
+}
