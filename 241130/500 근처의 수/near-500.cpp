7f88e665af443fc1b9ae0ecc107843500ada083a
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-11-30 23:14:51 +0900

    Title: 500 근처의 수, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 241130/500 근처의 수/near-500.cpp
+++ 241130/500 근처의 수/near-500.cpp
@@ -31,5 +31,23 @@ int main() {
 
     cout << downMax << " " << upMin;
     
+    /* 나 왜 이렇게 어렵게 했지.
+    // max_val : 500 미만의 가장 큰 수, min_val : 500 초과의 가장 작은 수
+	int max_val = 1;
+	int min_val = 1000;
+	
+	// 10개의 숫자들 중 500근처의 수들을 구합니다.
+	for(int i = 0; i < 10; i++) {
+		if(arr[i] < 500 && arr[i] > max_val)
+			max_val = arr[i];
+		
+		if(arr[i] > 500 && arr[i] < min_val)
+			min_val = arr[i];
+	}
+	
+    // 구한 값을 출력합니다.
+    cout << max_val << " " << min_val;
+    */
+
     return 0;
 }
