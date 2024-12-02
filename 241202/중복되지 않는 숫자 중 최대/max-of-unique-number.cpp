275384a5124008a9d888bab6acb642adf9f5fcfa
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-12-02 18:39:00 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241202/중복되지 않는 숫자 중 최대/max-of-unique-number.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int arr[1000];
+    bool dup[1000];
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        dup[i] = false;
+    }
+    
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (dup[i] || dup[j])
+                continue;
+            if (arr[i] == arr[j])
+            {
+                dup[i] = true;
+                dup[j] = true;
+            }
+        }
+    }
+
+    int max = -1;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > max && !dup[i])
+            max = arr[i];
+    }
+
+    cout << max;
+
+    return 0;
+}
