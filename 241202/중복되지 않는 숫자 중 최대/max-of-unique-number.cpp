3a3de4a1bc6e8f187a334c93dab86eb7fe5ac4fe
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-12-02 18:46:15 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241202/중복되지 않는 숫자 중 최대/max-of-unique-number.cpp
+++ 241202/중복되지 않는 숫자 중 최대/max-of-unique-number.cpp
@@ -27,6 +27,28 @@ int main() {
 
     int max = -1;
 
+    /* dup배열은 없어도 될거같았는데 정말 쉬운거였구나
+    // 최댓값 찾기
+    int max = -1;
+    for(int i = 0; i < n; i++) {
+        int curr_num = nums[i];
+        // 최대가 될 수 있는 후보입니다.
+        if(max < curr_num) {
+            // 갱신할 수 있는지 확인하기 위해 이 숫자의 등장 빈도를 셉니다.
+            int count = 0;
+            for(int j = 0; j < n; j++) {
+                if(nums[j] == curr_num) {
+                    count++;
+                }
+            }
+            // 이 숫자가 배열에서 유일할때만 갱신합니다.
+            if(count == 1) {
+                max = curr_num;
+            }
+        }
+    }
+    */
+
     for (int i = 0; i < n; i++)
     {
         if (arr[i] > max && !dup[i])
