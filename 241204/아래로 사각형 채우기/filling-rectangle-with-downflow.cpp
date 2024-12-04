afb336d7282604ebffc5d0f190dff6545b46b46f
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-12-04 19:25:08 +0900

    Title: 아래로 사각형 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241204/아래로 사각형 채우기/filling-rectangle-with-downflow.cpp
+++ 241204/아래로 사각형 채우기/filling-rectangle-with-downflow.cpp
@@ -10,9 +10,7 @@ int main() {
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
-        {
             arr[j][i] = num++;
-        }
     }
 
     for (int i = 0; i < n; i++)
