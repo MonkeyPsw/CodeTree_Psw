7d2fb2d914e57c7145494e78e2ef7a016b1ab68a
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-12-04 17:41:49 +0900

    Title: 두 배열의 곱, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 241204/두 배열의 곱/multiple-of-two-arrays.cpp
+++ 241204/두 배열의 곱/multiple-of-two-arrays.cpp
@@ -2,28 +2,32 @@
 using namespace std;
 
 int main() {
-    int arr[3][3];
+    int arr1[3][3];
+    int arr2[3][3];
+    int ans[3][3];
 
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
-            cin >> arr[i][j];
-    }
-
-    int num;
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            cin >> num;
-            arr[i][j] *= num;
-        }
+            cin >> arr1[i][j];
     }
 
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
-            cout << arr[i][j] << " ";
+            cin >> arr2[i][j];
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+            ans[i][j] = arr1[i][j] * arr2[i][j];
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+            cout << ans[i][j] << " ";
         cout << endl;
     }
 
