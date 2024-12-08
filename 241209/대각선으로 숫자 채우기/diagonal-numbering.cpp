5666f1e4d553cdffeb7e27edb9df8842b9704ec1
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-12-09 00:28:40 +0900

    Title: 대각선으로 숫자 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241209/대각선으로 숫자 채우기/diagonal-numbering.cpp
+++ 241209/대각선으로 숫자 채우기/diagonal-numbering.cpp
@@ -7,6 +7,7 @@ int main() {
 
     cin >> n >> m;
 
+    // 내 방법은 시간복잡도가 O(N^2*M + N*M^2) 맞나?
     for (int k = 0; k <= n + m; k++)
     {
         for (int i = 0; i < n; i++)
@@ -17,8 +18,39 @@ int main() {
                     arr[i][j] = num++;
             }
         }
-
     }
+
+    /* 해설은 O(N*M)
+    // Step 1:
+    for(int start_col = 0; start_col < m; start_col++) {
+        int curr_row = 0;
+		int curr_col = start_col;
+
+		while(curr_col >= 0 && curr_row < n) {
+			answer[curr_row][curr_col] = count;
+
+			// 변수 업데이트 :
+			curr_row++;
+			curr_col--;
+			count++;
+		}
+    }
+
+    // Step 2:
+    for(int start_row = 1; start_row < n; start_row++) {
+        int curr_row = start_row;
+		int curr_col = m - 1;
+
+        while(curr_col >= 0 && curr_row < n) {
+			answer[curr_row][curr_col] = count;
+
+			// 변수 업데이트 :
+			curr_row++;
+			curr_col--;
+			count++;
+		}
+    }
+    */
 
     for (int i = 0; i < n; i++)
     {
