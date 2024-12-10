c8141e6f74c705c5b306617852641a81783bf418
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-12-10 21:58:02 +0900

    Title: 가장 짧은 문자열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241210/가장 짧은 문자열/shortest-string.cpp
+++ 241210/가장 짧은 문자열/shortest-string.cpp
@@ -36,5 +36,15 @@ int main() {
             cout << c - a;
     }
 
+    /* 해설 방식은 그냥 단순하게 했네
+    // 세 문자열 중 가장 긴 길이와 가장 짧은 길이를 구합니다.
+    int mx = len1;
+    if(mx < len2) mx = len2;
+    if(mx < len3) mx = len3;
+    int mn = len1;
+    if(mn > len2) mn = len2;
+    if(mn > len3) mn = len3;
+    */
+
     return 0;
 }
