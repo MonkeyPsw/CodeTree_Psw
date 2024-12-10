b90de10606c3e8db0ab9529cb9bea155cccb80c4
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-12-10 22:11:33 +0900

    Title: 공백을 기준으로 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241210/공백을 기준으로 출력/output-based-on-space.cpp
+++ 241210/공백을 기준으로 출력/output-based-on-space.cpp
@@ -4,23 +4,21 @@ using namespace std;
 
 int main() {
     string str1, str2;
-    char c = ' ';
     getline(cin, str1);
     getline(cin, str2);
 
     for (int i = 0; i < str1.length(); i++)
     {
-        if (str1[i] == c)
+        if (str1[i] == ' ')
             continue;
         cout << str1[i];
     }
     for (int i = 0; i < str2.length(); i++)
     {
-        if (str2[i] == c)
+        if (str2[i] == ' ')
             continue;
         cout << str2[i];
     }
-
 
     return 0;
 }
