7b6b292d3d7c1c7d43e718028f50c3e5b1cc2d8c
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-12-12 21:42:27 +0900

    Title: 문자열 범위 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241212/문자열 범위 출력하기 2/print-string-in-range-2.cpp
+++ 241212/문자열 범위 출력하기 2/print-string-in-range-2.cpp
@@ -18,6 +18,17 @@ int main() {
         for (int i = arr.length() - 1; i >= arr.length() - n; i--)
             cout << arr[i];
     }
+    
+    /*
+    // 문자열의 맨 뒤에서부터 주어진 개수만큼 출력합니다.
+	for(int i = len - 1; i >= 0; i--) {
+		// 주어진 개수만큼 출력했을 경우 for문을 나갑니다.
+		if(cnt >= a)
+			break;
+		cout << str[i];
+		cnt++;
+	}
+    */
 
     return 0;
 }
