25802b93f9805fab08918823898c9f319b0d84ab
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-12-12 21:27:01 +0900

    Title: 특정 문자로 끝나는 문자열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241212/특정 문자로 끝나는 문자열/string-ending-with-specific-character.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string arr[10];
+    char c;
+    
+    for (int i = 0; i < 10; i++)
+        cin >> arr[i];
+
+    cin >> c;
+
+    int cnt = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        if (arr[i][arr[i].length() - 1] == c)
+        {
+            cout << arr[i] << endl;
+            cnt++;
+        }
+    }
+    
+    if (!cnt)
+        cout << "None";
+
+    return 0;
+}
