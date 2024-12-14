c92bddba7e7932bedad8d862915382cdc03b0791
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-12-15 02:24:41 +0900

    Title: 두 문자열을 이어붙였을 때, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241215/두 문자열을 이어붙였을 때/when-two-strings-are-concatenated.cpp
+++ 241215/두 문자열을 이어붙였을 때/when-two-strings-are-concatenated.cpp
@@ -6,5 +6,18 @@ int main() {
     string str1, str2;
     cin >> str1 >> str2;
     cout << (str1 + str2 == str2 + str1 ? "true" : "false");
+    /* 문자열 비교는 아직이었군
+    // 문자열 c에 문자열 a와 b를 붙입니다.
+    string c = a + b;
+	
+	// 문자열 d에 문자열 b와 a를 붙입니다.
+    string d = b + a;
+	
+	// 두 문자열이 같다면 true를, 같지 않다면 false를 출력합니다.
+	if(c == d)
+        cout << "true";
+    else
+        cout << "false";
+    */
     return 0;
 }
