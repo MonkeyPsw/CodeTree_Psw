beae447f9119b0b3d459fb0dae9aca32734352d7
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-12-30 18:24:18 +0900

    Title: 정수만 추출하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241230/정수만 추출하기/extract-only-integers.cpp
+++ 241230/정수만 추출하기/extract-only-integers.cpp
@@ -24,5 +24,19 @@ int main() {
 
     cout << a_int + b_int;
 
+    /* 해설에서는 인덱스를 찾아서 따로 변환
+    // a의 정수로 변환 가능한 최대 인덱스를 찾습니다.
+	for(int i = 0; i < len_a; i++)
+		if(a[i] <= '9' && a[i] >= '0')
+			idx1++;
+		else break;
+	
+	// b의 정수로 변환 가능한 최대 인덱스를 찾습니다.
+	for(int i = 0; i < len_b; i++)
+		if(b[i] <= '9' && b[i] >= '0')
+			idx2++;
+		else break;
+    */
+
     return 0;
 }
