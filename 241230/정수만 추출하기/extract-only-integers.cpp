99bf2100458edb1cec88630f53b34b658ceadb0b
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-12-30 18:20:59 +0900

    Title: 정수만 추출하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241230/정수만 추출하기/extract-only-integers.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a, b;
+    int a_int, b_int;
+
+    cin >> a >> b;
+
+    for (int i = 0; i < a.length(); i++)
+    {
+        if (!isdigit(a[i]))
+            a_int = stoi(a.substr(0, i));
+    }
+
+    for (int i = 0; i < b.length(); i++)
+    {
+        if (!isdigit(b[i]))
+            b_int = stoi(b.substr(0, i));
+    }
+
+    cout << a_int + b_int;
+
+    return 0;
+}
