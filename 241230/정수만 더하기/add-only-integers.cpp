b5df91aeb717afaca6291ff85b890395eed92140
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-12-30 18:12:05 +0900

    Title: 정수만 더하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241230/정수만 더하기/add-only-integers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main() {
+    string str;
+    int sum = 0;
+
+    cin >> str;
+
+    for (int i = 0; i < str.length(); i++)
+    {
+		if (str[i] >= '0' && str[i] <= '9')
+			sum += (int)str[i] - '0';
+    }
+
+    cout << sum;
+
+    return 0;
+}
