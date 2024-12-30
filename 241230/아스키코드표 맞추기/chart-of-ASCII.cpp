bf68465faa05c18d5cafbaffa6637ffda6f93a92
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-12-30 17:41:50 +0900

    Title: 아스키코드표 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241230/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> n;
+        char c = (char)n;
+        cout << c << " ";
+    }
+    
+    return 0;
+}
