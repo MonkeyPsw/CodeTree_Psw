c92cada2c9097f93f252e42e7cb8f0252285af28
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2024-12-30 18:22:54 +0900

    Title: 정수만 추출하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241230/정수만 추출하기/extract-only-integers.cpp
+++ 241230/정수만 추출하기/extract-only-integers.cpp
@@ -10,12 +10,14 @@ int main() {
 
     for (int i = 0; i < a.length(); i++)
     {
+        a_int = stoi(a);
         if (!isdigit(a[i]))
             a_int = stoi(a.substr(0, i));
     }
 
     for (int i = 0; i < b.length(); i++)
     {
+        b_int = stoi(b);
         if (!isdigit(b[i]))
             b_int = stoi(b.substr(0, i));
     }
