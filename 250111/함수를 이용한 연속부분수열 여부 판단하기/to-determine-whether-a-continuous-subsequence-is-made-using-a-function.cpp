74457328f9e0bb06afa8ed1af56a3431bb24f198
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-01-11 21:30:22 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250111/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
+++ 250111/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
@@ -5,6 +5,27 @@ using namespace std;
 int n1, n2;
 int a[100], b[100];
 
+/* i < n1 - n2로 했다가 틀렸었는데 그냥 부등호 바꿔주면 됐구나
+// n번째 인덱스부터 시작하는 n2길이의 a수열과
+// b수열이 완전히 일치하는지 확인합니다..
+bool IsSame(int n) {
+    for(int i = 0; i < n2; i++)
+        if(a[i + n] != b[i])
+            return false;
+
+    return true;
+}
+
+// b가 a의 연속부분수열인지 확인합니다.
+bool IsSubsequence() {
+    for(int i = 0; i <= n1 - n2; i++)
+        if(IsSame(i))
+            return true;
+    
+    return false;
+}
+*/
+
 bool IsSame(int x, int y)
 {
     if (x == y)
