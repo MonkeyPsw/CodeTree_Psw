4823b357e400fc376eec59189d9951d71e2c2dc5
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-01-11 21:23:00 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250111/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int n1, n2;
+int a[100], b[100];
+
+bool IsSame(int x, int y)
+{
+    if (x == y)
+        return true;
+    return false;
+}
+
+int main() {
+    cin >> n1 >> n2;
+
+    bool isSub = false;
+
+    for (int i = 0; i < n1; i++) cin >> a[i];
+
+    for (int i = 0; i < n2; i++) cin >> b[i];
+
+    for (int i = 0; i < n1 - n2; i++)
+    {
+        if (IsSame(a[i], b[0]))
+        {
+            if (isSub)
+			    break;
+
+            for (int j = 0; j < n2; j++)
+            {
+                if (!IsSame(a[i + j], b[j]))
+                {
+                    isSub = false;
+                    break;
+                }
+                else
+                    isSub = true;
+            }
+        }
+    }
+
+    cout << (isSub ? "Yes" : "No");
+
+    return 0;
+}
