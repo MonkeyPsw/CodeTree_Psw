847bf7606d2dc01d915b3d06fbc2e046dcf78a4e
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-01-11 21:57:55 +0900

    Title: 2021년 날짜의 유무, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250111/2021년 날짜의 유무/with-or-without-2021.cpp
+++ 250111/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -4,6 +4,26 @@ using namespace std;
 
 int M, D;
 
+/* 이게 더 낫군
+// 윤년이 아닐 때 m번째 달의 마지막 날을 반환하는 함수를 작성합니다.
+int LastDayNumber(int m) {
+    if(m == 2)
+        return 28;
+    if(m == 4 || m == 6 || m == 9 || m == 11)
+        return 30;
+    
+    return 31;
+}
+
+// 윤년이 아닐 때 m월 d일이 존재하는지 여부를 확인하는 함수를 작성합니다.
+bool JudgeDay(int m, int d) {
+    if(m <= 12 && d <= LastDayNumber(m))
+        return true;
+    
+    return false;
+}
+*/
+
 // 31일 1, 3, 5, 7, 8, 10, 12
 // 30일 4, 6, 9, 11
 // 28일 2
