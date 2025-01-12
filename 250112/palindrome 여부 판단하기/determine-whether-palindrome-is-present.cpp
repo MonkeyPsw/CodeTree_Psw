580782b6cad9bd625892f5a3f35b3b0b53106ec9
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-01-12 19:37:16 +0900

    Title: palindrome 여부 판단하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250112/palindrome 여부 판단하기/determine-whether-palindrome-is-present.cpp
+++ 250112/palindrome 여부 판단하기/determine-whether-palindrome-is-present.cpp
@@ -5,6 +5,16 @@ using namespace std;
 
 string A;
 
+/* 속도가 매우 중요하다면 이런식으로 가운데를 기준으로 대칭문자 확인
+bool Palindrome(string &s) {
+    for(int i = 0; i < (int) s.size(); i++)
+        if(s[i] != s[(int) s.size() - i - 1])
+            return false;
+    
+    return true;
+}
+*/
+
 void IsPalindrome(string &s)
 {
     string rev_s;
