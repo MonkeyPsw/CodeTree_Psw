fa4a745b9c3cd1127b544738f1b1c75c78625cea
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-01-14 22:14:57 +0900

    Title: 2개 이상의 알파벳, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250114/2개 이상의 알파벳/more-than-one-alphabet.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string A;
+
+bool IsManyAPB(string &s)
+{
+    char c = s[0];
+
+    for (int i = 1; i < s.length(); i++)
+    {
+        if (c != s[i])
+            return true;
+    }
+
+    return false;
+}
+
+int main() {
+    cin >> A;
+
+    cout << (IsManyAPB(A) ? "Yes" : "No");
+
+    return 0;
+}
