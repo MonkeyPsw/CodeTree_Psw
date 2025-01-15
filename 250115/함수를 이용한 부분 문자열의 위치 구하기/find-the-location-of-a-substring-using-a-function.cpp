11612057c9b10aa508e02a561e49ab487bf6f895
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-01-15 17:53:29 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250115/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.cpp
+++ 250115/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.cpp
@@ -6,6 +6,41 @@ using namespace std;
 string text;
 string pattern;
 
+/*
+bool IsSubStr(int start_idx) {
+    int n = (int) text.size();
+    int m = (int) pattern.size();
+
+    // 만약 pattern을 매칭 시키기에
+    // text 문자열 범위를 초과하게 된다면
+    // 부분 문자열이 될 수 없으므로 false를 반환합니다.
+    if(start_idx + m - 1 >= n)
+        return false;
+
+    for(int j = 0; j < m; j++) {
+        // 하나라도 다르다면, 부분 문자열이 아니므로 false를 반환합니다.
+        if(text[start_idx + j] != pattern[j])
+            return false;
+    }
+
+    // 전부 일치한다면 부분 문자열이므로 true를 반환합니다.
+    return true;
+}
+
+// 부분 문자열의 위치를 찾아 반환합니다.
+int FindIndex() {
+    int n = (int) text.size();
+    for(int i = 0; i < n; i++) {
+        // i번째를 시작으로 부분 문자열이 된다면, 해당 위치를 반환합니다.
+        if(IsSubStr(i))
+            return i;
+    }
+
+    // 없다면, -1을 반환합니다.
+    return -1;
+}
+*/
+
 int FindString()
 {
     if (text.find(pattern) != string::npos)
