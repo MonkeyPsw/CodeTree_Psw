0fe4af416191bc2c56954a2443df706e40715fc1
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-01-21 17:53:14 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250121/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.cpp
+++ 250121/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.cpp
@@ -5,6 +5,20 @@ using namespace std;
 int n;
 int arr[100];
 
+/* 이건 뭔데
+#include <algorithm>
+
+// a번째 까지 인덱스의 숫자 중에 가장 큰 값을 반환합니다.
+int MaxValue(int a) {
+    if(a == 0)
+        return arr[0];
+
+    return max(MaxValue(a - 1), arr[a]);
+}
+
+cout << MaxValue(n - 1);
+*/
+
 int FindMax(int arr[], int length)
 {
     if (length == 1)
