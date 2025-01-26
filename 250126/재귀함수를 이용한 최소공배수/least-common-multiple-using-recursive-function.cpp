abaa0771d7d0843821e78d711a58d8e1cf29131a
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-01-26 21:19:49 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250126/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.cpp
+++ 250126/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.cpp
@@ -5,6 +5,30 @@ using namespace std;
 int n;
 int arr[10];
 
+/*
+// 두 수간의 최소공배수를 구하여 반환합니다.
+int LCM(int a, int b) {
+    int gcd = 1;
+    for(int i = 1; i <= min(a, b); i++) {
+        if(a % i == 0 && b % i == 0)
+            gcd = i;
+    }
+
+    return a * b / gcd;
+}
+
+// index번째 까지 인덱스의 숫자 중에 가장 큰 값을 반환합니다.
+int GetLCMAll(int index) {
+    // 남은 원소가 1개라면 그 자신이 답이 됩니다.
+    if(index == 1)
+        return arr[1];
+
+    // 1번째 ~ index - 1번째 원소의 최소공배수를 구한 결과와
+    // 현재 index 원소의 최소공배수를 구하여 반환합니다.
+    return LCM(GetLCMAll(index - 1), arr[index]);
+}
+*/
+
 int GetGCD(int a, int b)
 {
     return b ? GetGCD(b, a % b) : a;
