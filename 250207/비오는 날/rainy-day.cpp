bc3481bbb02e1617820ac675c9e45f15526a08ff
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-02-07 22:07:10 +0900

    Title: 비오는 날, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250207/비오는 날/rainy-day.cpp
+++ 250207/비오는 날/rainy-day.cpp
@@ -21,6 +21,22 @@ class Data {
         Data() {}
 };
 
+/* 입력코드를 아예 안썼네
+// Forecast 정보를 나타내는 클래스 선언
+class Forecast {
+    public:
+        string date, day, weather;
+        Forecast(string date, string day, string weather) {
+            this->date = date;
+            this->day = day;
+            this->weather = weather;
+        }
+};
+
+Forecast ans = Forecast("9999-99-99", "", "");
+*/
+
+
 int main() {
     Data d[100];
 
@@ -51,6 +67,22 @@ int main() {
         if (d[idx].date > d[i].date)
             idx = i;
     }
+
+    /* 입력코드 안쓸거면 이렇게 했지?
+    for(int i = 1; i <= n; i++) {
+            string date, day, weather;
+            cin >> date >> day >> weather;
+
+            // Forecast 객체를 만들어 줍니다.
+            Forecast f = Forecast(date, day, weather);
+            if(weather == "Rain")
+                // 비가 오는 경우 가장 최근인지 확인하고,
+                // 가장 최근일 경우 정답을 업데이트합니다.
+                if(ans.date >= f.date)
+                    ans = f;
+        }
+
+    */
 
     cout << d[idx].date << " " << d[idx].day << " " << d[idx].weather;
 
