e368b4a4e8b80f85509051fba12f72347f11595e
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-03-11 18:21:11 +0900

    Title: 벌금은 누구에게, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250311/벌금은 누구에게/who-will-pay.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int N, M, K;
+int penalty[10000];
+int student[101];
+
+int main() {
+    int ans = -1;
+
+    cin >> N >> M >> K;
+
+    for (int i = 0; i < M; i++) {
+        if (ans > 0)
+            break;
+
+        cin >> penalty[i];
+
+        student[penalty[i]]++;
+
+        for (int j = 0; j < N; j++)
+        {
+            if (student[j] >= K)
+            {
+                ans = student[j] - 1;
+                break;
+            }
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}
