0d5039d0f862d537eb684d5575e06a0d67dfc56b
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-03-14 22:27:44 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250314/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N, K, P, T;
+int t[250];
+int x[250];
+int y[250];
+int inf[101]; // 감염 여부 배열 : 0, 1
+int cnt[101]; // 감염 횟수 배열 : 0 ~ K
+
+class HandShake {
+    public :
+        int t, x, y;
+
+        HandShake(int t, int x, int y) {
+            this->t = t;
+            this->x = x;
+            this->y = y;
+        }
+
+        HandShake() {}
+};
+
+bool cmpTime(HandShake a, HandShake b)
+{
+    return a.t < b.t;
+}
+
+int main() {
+    HandShake h[250];
+
+    cin >> N >> K >> P >> T;
+
+    for (int i = 0; i < T; i++) {
+        cin >> t[i] >> x[i] >> y[i];
+
+        h[i] = HandShake(t[i], x[i], y[i]);
+    }
+
+    sort(h, h + T, cmpTime);
+
+    inf[P] = 1;
+
+    for (int i = 0; i < T; i++)
+    {
+        if (inf[h[i].x])
+        {
+            if (cnt[h[i].x] < K)
+            {
+                cnt[h[i].x]++;
+                inf[h[i].y] = 1;
+            }
+        }
+
+        if (inf[h[i].y])
+        {
+            if (cnt[h[i].y] < K)
+            {
+                cnt[h[i].y]++;
+                inf[h[i].x] = 1;
+            }
+        }
+    }
+
+    for (int i = 1; i <= N; i++)
+        cout << inf[i];
+
+    return 0;
+}
