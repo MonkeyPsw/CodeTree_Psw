0860b7bc36c39561207702d30509c4b38b19da19
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-03-20 00:20:00 +0900

    Title: 되돌아오기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250320/되돌아오기/come-back.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+char dir[100];
+int dist[100];
+int dx[4] = {1, 0, -1, 0}, dy[4] = {0, -1, 0, 1};
+int x, y;
+int d, cur;
+//W서 S남 N북 E동
+
+int main() {
+    cin >> N;
+
+    int cnt = -1;
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> dir[i] >> dist[i];
+
+        switch (dir[i])
+        {
+            case 'E' :
+                d = 0;
+                break;
+            case 'S' :
+                d = 1;
+                break;
+            case 'W' :
+                d = 2;
+                break;
+            case 'N' :
+                d = 3;
+                break;
+        }
+
+        for (int j = 0; j < dist[i]; j++)
+        {
+            x += dx[d];
+            y += dy[d];
+            cur++;
+
+            if (!x && !y)
+            {
+                cnt = cur;
+                break;
+            }
+        }
+    }
+
+    cout << cnt;
+
+    return 0;
+}
