83954a5b291d5ed26144063cea8f0c04b943da1f
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-03-26 20:55:34 +0900

    Title: 모이자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250326/모이자/gather.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int n;
+int A[100];
+int ans = INT_MAX;
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+
+
+    for (int i = 0; i < n; i++)
+    {
+        int cnt = 0;
+        for (int j = 0; j < n; j++)
+            cnt += abs(i - j) * A[j];
+
+        if (ans > cnt)
+            ans = cnt;
+    }
+
+    
+
+    return 0;
+}
