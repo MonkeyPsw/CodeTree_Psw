76643373817ced489c7aa9bb4c06de77c2d24875
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-03-26 21:05:24 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250326/괄호 쌍 만들어주기 3/pair-parentheses-3.cpp
+++ 250326/괄호 쌍 만들어주기 3/pair-parentheses-3.cpp
@@ -14,10 +14,19 @@ int main() {
         if (A[i] == '(')
         {
             for (int j = i + 1; j < A.length(); j++)
+            {
                 if (A[j] == ')')
                     cnt++;
+            }
         }
     }
+
+    /* 완전탐색은 이게 더 어울리나 어차피 둘다 O(N²)라서 흠
+    for(int i = 0; i < n; i++)
+        for(int j = i + 1; j < n; j++)
+            if(str[i] == '(' && str[j] == ')')
+                cnt++;
+    */
 
     cout << cnt;
 
