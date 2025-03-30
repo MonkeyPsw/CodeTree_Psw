25415d7aba409b060fc3a487afc1eebed97e2c17
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-03-30 22:01:02 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250330/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.cpp
+++ 250330/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.cpp
@@ -29,6 +29,24 @@ int main() {
         ans = min(ans, cnt);
     }
 
+    /* 이런식으로 할랬는데 몰루
+    // 각 i번째 체크포인트를 건너 뛰었을 때의 거리를 구해줍니다.
+	int ans = INT_MAX;
+    for(int i = 1; i < n - 1; i++) {
+		// 거리를 구합니다.
+		int dist = 0;
+		int prev_idx = 0;
+    	for(int j = 1; j < n; j++) {
+			if(j == i) continue;
+        	dist += abs(x[prev_idx] - x[j]) + abs(y[prev_idx] - y[j]);
+			prev_idx = j;
+		}
+		
+		// 가능한 거리 중 최솟값을 구해줍니다.
+		ans = min(ans, dist);
+	}
+    */
+
     cout << ans;
 
     return 0;
