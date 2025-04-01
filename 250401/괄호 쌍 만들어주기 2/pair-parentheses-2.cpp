00a93c7f5477e3388581f1af211020e257a343e3
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-04-01 16:35:27 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250401/괄호 쌍 만들어주기 2/pair-parentheses-2.cpp
+++ 250401/괄호 쌍 만들어주기 2/pair-parentheses-2.cpp
@@ -9,6 +9,19 @@ int cnt;
 int main() {
     cin >> A;
 
+    for (int i = 0; i < A.length() - 1; i++)
+    {
+        if (A[i] == '(' && A[i + 1] == '(')
+        {
+            for (int j = i + 1; j < A.length() - 1; j++)
+            {
+                if (A[j] == ')' && A[j + 1] == ')')
+                    cnt++;
+            }
+        }
+    }
+
+    /* 이렇게 해도 정답이더라
     for (int i = 0; i < A.length(); i++)
     {
         for (int j = i + 2; j < A.length(); j++)
@@ -20,6 +33,7 @@ int main() {
             }
         }
     }
+    */
 
     cout << cnt;
 
