be806f5170492c35d24248c34b7ddeda39ad91a2
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-04-05 02:08:58 +0900

    Title: 오목, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250405/오목/O-mok.cpp
@@ -0,0 +1,193 @@
+#include <iostream>
+
+using namespace std;
+
+int board[19][19];
+
+/* 이전 풀이
+int black[4], white[4]; // 0가로 1세로 2오른쪽아래대각\ 3왼쪽아래대각/
+bool isAns, isBlack;
+int x, y;
+*/
+
+// dx dy 생각은 했는데 안썼다 나중에 다시 해볼까
+/*
+int dx[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+int dy[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+*/
+// 근데 굳이 8방향 다 해야하나? 그건 또 몰루
+// 왼쪽아래 아래 오른쪽아래 오른쪽만 해도될듯?
+
+// dx dy 테크닉 활용
+int dx[4] = {1, 1, 1, 0};
+int dy[4] = {-1, 0, 1, 1};
+
+bool InRange(int x, int y)
+{
+    return 0 <= x && x < 19 && 0 <= y && y < 19;
+}
+
+/* 이전 풀이
+void ResetArr(bool color)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        if (color)
+            black[i] = 0;
+        else
+            white[i] = 0;
+    }
+}
+*/
+
+int main() {
+    for (int i = 0; i < 19; i++)
+        for (int j = 0; j < 19; j++) cin >> board[i][j];
+
+    // dx dy 테크닉 활용
+    for (int i = 0; i < 19; i++)
+    {
+        for (int j = 0; j < 19; j++)
+        {
+            if (board[i][j])
+            {
+                for (int k = 0; k < 4; k++)
+                {
+                    int cnt = 0;
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+
+                    if (!InRange(nx, ny))
+                        continue;
+                    
+                    while (InRange(nx, ny) && board[nx - dx[k]][ny - dy[k]] == board[nx][ny])
+                    {
+                        cnt++;
+
+                        if (cnt >= 4)
+                        {
+                            cout << board[i][j] << endl;
+                            cout << i + dx[k] * 2 + 1 << " ";
+                            cout << j + dy[k] * 2 + 1;
+                            return 0;
+                        }
+
+                        nx += dx[k];
+                        ny += dy[k];
+                    }
+                }
+            }
+        }
+    }
+
+    /* 이전 풀이
+    for (int i = 0; i < 19; i++)
+    {
+        for (int j = 0; j < 19; j++)
+        {
+            if (isAns)
+            {
+                cout << (isBlack ? 1 : 2) << endl;
+                cout << x << " " << y;
+                return 0;
+            }
+
+            if (board[i][j] == 1)
+            {
+                for (int n = 1; n < 5; n++)
+                {
+                    if (board[i + n][j] == 1)
+                        black[0]++;
+                    if (board[i][j + n] == 1)
+                        black[1]++;
+                    if (board[i + n][j + n] == 1)
+                        black[2]++;
+                    if (board[i - n][j + n] == 1)
+                        black[3]++;
+                }
+
+                for (int n = 0; n < 4; n++)
+                {
+                    if (black[n] == 4)
+                    {
+                        if (n == 0)
+                        {
+                            x = i + 3;
+                            y = j + 1;
+                        }
+                        else if (n == 1)
+                        {
+                            x = i + 1;
+                            y = j + 3;
+                        }
+                        else if (n == 2)
+                        {
+                            x = i + 3;
+                            y = j + 3;
+                        }
+                        else
+                        {
+                            x = i - 1;
+                            y = j + 3;
+                        }
+
+                        isAns = true;
+                        isBlack = true;
+                    }
+                }
+
+                ResetArr(true);
+            }
+            else if (board[i][j] == 2)
+            {
+                for (int n = 1; n < 5; n++)
+                {
+                    if (board[i + n][j] == 2)
+                        white[0]++;
+                    if (board[i][j + n] == 2)
+                        white[1]++;
+                    if (board[i + n][j + n] == 2)
+                        white[2]++;
+                    if (board[i - n][j + n] == 2)
+                        white[3]++;
+                }
+
+                for (int n = 0; n < 4; n++)
+                {
+                    if (white[n] == 4)
+                    {
+                        if (n == 0)
+                        {
+                            x = i + 3;
+                            y = j + 1;
+                        }
+                        else if (n == 1)
+                        {
+                            x = i + 1;
+                            y = j + 3;
+                        }
+                        else if (n == 2)
+                        {
+                            x = i + 3;
+                            y = j + 3;
+                        }
+                        else
+                        {
+                            x = i - 1;
+                            y = j + 3;
+                        }
+
+                        isAns = true;
+                    }
+                }
+
+                ResetArr(false);
+            }
+        }
+    }
+    */
+
+    cout << 0;
+
+    return 0;
+}
