ae7cbd363e43189a9b57d9e75fe14e89d443d817
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-04-05 20:09:33 +0900

    Title: 최고의 13위치 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250405/최고의 13위치 2/best-place-of-13-2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <climits>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+int arr[20][20];
+bool check[20][20];
+int ans = INT_MIN;
+
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++) cin >> arr[i][j];
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N - 2; j++)
+        {
+            for (int k = 0; k < N; k++)
+            {
+                for (int l = 0; l < N - 2; l++)
+                {
+                    if (i == k && l < j + 2)
+                        continue;
+
+                    int sum1 = arr[i][j] + arr[i][j + 1] + arr[i][j + 2];
+                    int sum2 = arr[k][l] + arr[k][l + 1] + arr[k][l + 2];
+                    ans = max(ans, sum1 + sum2);
+                }
+            }
+        }
+    }
+    
+    cout << ans;
+
+    return 0;
+}
