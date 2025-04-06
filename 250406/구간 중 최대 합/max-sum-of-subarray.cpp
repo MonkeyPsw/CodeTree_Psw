c82cb38acd32851e1a2386f26568ba60b2c72f88
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-04-06 22:31:12 +0900

    Title: 구간 중 최대 합, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250406/구간 중 최대 합/max-sum-of-subarray.cpp
+++ 250406/구간 중 최대 합/max-sum-of-subarray.cpp
@@ -14,7 +14,7 @@ int main() {
         cin >> arr[i];
     }
 
-    for (int i = 0; i < n - k; i++)
+    for (int i = 0; i <= n - k; i++)
     {
         int sum = 0;
         for (int j = i; j < i + k; j++)
