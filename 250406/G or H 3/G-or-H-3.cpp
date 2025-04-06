e95736341dc72f9a3877cbc59e32557264dfea70
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-04-06 23:28:18 +0900

    Title: G or H 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250406/G or H 3/G-or-H-3.cpp
+++ 250406/G or H 3/G-or-H-3.cpp
@@ -8,6 +8,7 @@ int N, K;
 int x[100];
 char c[100];
 char line[10001];
+int val = INT_MIN;
 int ans = INT_MIN;
 
 int main() {
@@ -17,9 +18,10 @@ int main() {
         cin >> x[i] >> c[i];
 
         line[x[i]] = c[i];
+        val = max(val, x[i]);
     }
 
-    for (int i = 1; i <= N - K + 1; i++)
+    for (int i = 1; i <= val - K + 1; i++)
     {
         int cnt = 0;
         
@@ -30,7 +32,7 @@ int main() {
             else if (line[j] == 'H')
                 cnt += 2;
         }
-        
+
         ans = max(ans, cnt);
     }
 
