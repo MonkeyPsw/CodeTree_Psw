6472290bc7a329a08b87de276ba7d6dfe7f41538
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-04-14 17:35:54 +0900

    Title: 개발팀의 능력, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250414/개발팀의 능력/the-capabilities-of-the-development-team.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <climits>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int arr[5];
+int ans = INT_MAX;
+
+int GetDiff(int i, int j, int k, int l)
+{
+    int sum1 = arr[i] + arr[j];
+    int sum2 = arr[k] + arr[l];
+    int allSum = 0;
+
+    if (sum1 == sum2)
+        return INT_MAX;
+
+    for (int n = 0; n < 6; n++)
+        allSum += arr[n];
+
+    allSum -= sum1;
+    allSum -= sum2;
+
+    if (allSum == sum1 || allSum == sum2)
+        return INT_MAX;
+
+    return abs(max(max(sum1, sum2), allSum) - min(min(sum1, sum2), allSum));
+}
+
+int main() {
+    for (int i = 0; i < 5; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < 6; i++)
+    {
+        for (int j = 0; j < 6; j++)
+        {
+            for (int k = 0; k < 6; k++)
+            {
+                for (int l = 0; l < 6; l++)
+                {
+                    if (i == j || i == k || i == l ||
+                        j == k || j == l || k == l)
+                            continue;
+                    ans = min(ans, GetDiff(i, j, k, l));
+                }
+            }
+        }
+    }
+
+    if (ans == INT_MAX)
+        cout << -1;
+    else
+        cout << ans;
+
+    return 0;
+}
