b3c02a476191d986e2f0dc112fbfb9b4dec6701f
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-04-19 23:22:25 +0900

    Title: 변수 값 복사하기 3, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250419/변수 값 복사하기 3/copying-variable-values-3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 1, b = 5, c = 3;
+
+    a = c;
+
+    a += c;
+
+    b -= c;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c;
+
+    return 0;
+}
