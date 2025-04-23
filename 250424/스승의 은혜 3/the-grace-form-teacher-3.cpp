4110ae39cba6c25628d67131cfb7c93c3d39031a
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-04-24 01:40:49 +0900

    Title: 스승의 은혜 3, Time: 20ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250424/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N, B;
+int P[1000];
+int S[1000];
+int ans;
+
+class Gift {
+    public :
+        int price, ship;
+
+        Gift(int p, int s) {
+            price = p;
+            ship = s;
+        }
+
+        Gift() {}
+};
+
+bool CmpPrice(Gift a, Gift b)
+{
+    return a.price + a.ship < b.price + b.ship;
+}
+
+int main() {
+    Gift g[1000];
+
+    cin >> N >> B;
+
+    for (int i = 0; i < N; i++) {
+        cin >> P[i] >> S[i];
+
+        g[i] = Gift(P[i], S[i]);
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        int sum = 0;
+        int cnt = 0;
+
+        g[i].price /= 2;
+
+        sort(g, g + N, CmpPrice);
+
+        for (int j = 0; j < N; j++)
+        {
+            sum += g[j].price + g[j].ship;
+
+            if (sum > B)
+                break;
+
+            cnt++;
+        }
+
+        ans = max(ans, cnt);
+
+        g[i].price *= 2;
+    }
+
+    cout << ans;
+
+    return 0;
+}
