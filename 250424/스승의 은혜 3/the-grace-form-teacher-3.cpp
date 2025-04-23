d3af967f4fad7f0f4d4f0ff612d661b75e0930e9
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-04-24 02:44:23 +0900

    Title: 스승의 은혜 3, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250424/스승의 은혜 3/the-grace-form-teacher-3.cpp
+++ 250424/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -8,6 +8,7 @@ int P[1000];
 int S[1000];
 int ans;
 
+/* 너무 어렵게 풀었어
 class Gift {
     public :
         int price, ship, index;
@@ -73,6 +74,47 @@ int main() {
                 break;
             }
         }
+    }
+
+    cout << ans;
+
+    return 0;
+}
+*/
+
+int main() {
+    cin >> N >> B;
+
+    for (int i = 0; i < N; i++) {
+        cin >> P[i] >> S[i];
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        int sum = 0;
+        int cnt = 0;
+        int arr[1000] = {};
+
+        P[i] /= 2;
+
+        for (int j = 0; j < N; j++)
+            arr[j] = P[j] + S[j];
+
+        sort(arr, arr + N);
+
+        for (int j = 0; j < N; j++)
+        {
+            sum += arr[j];
+
+            if (sum > B)
+                break;
+
+            cnt++;
+        }
+
+        ans = max(ans, cnt);
+
+        P[i] *= 2;
     }
 
     cout << ans;
