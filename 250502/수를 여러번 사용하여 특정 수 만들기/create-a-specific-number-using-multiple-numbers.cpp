b70be3162a7cd840f79a01bdccc0b3a51c8ec9f8
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-05-02 23:25:36 +0900

    Title: 수를 여러번 사용하여 특정 수 만들기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250502/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.cpp
+++ 250502/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.cpp
@@ -18,6 +18,22 @@ int main() {
         }
     }
 
+    /* for문 한번으로 되는구나 시간을 생각하면
+    // a를 몇 회 사용할지 전부 시도해 봅니다.
+    // 모든 경우의 수에 대해 최대가 되도록 하는 수를 계산합니다.
+    for(int i = 0; i * a <= c; i++) {
+        // a를 i회 사용합니다.
+        int cnt = a * i;
+
+        // 값을 최대로 하기 위해 b를 몇회 사용해야 하는지 계산합니다.
+        int num_b = (c - cnt) / b;
+
+        cnt += num_b * b;
+        
+        ans = max(ans, cnt);
+    }
+    */
+
     cout << ans;
 
     return 0;
