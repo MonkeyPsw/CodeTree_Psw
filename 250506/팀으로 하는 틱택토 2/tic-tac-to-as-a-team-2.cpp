68a458a51b472e66a7ca719aa85f56a921fb7546
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-05-06 23:04:56 +0900

    Title: 팀으로 하는 틱택토 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250506/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.cpp
+++ 250506/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.cpp
@@ -7,13 +7,30 @@ string inp[3];
 int ans;
 bool arr[10];
 
+bool checkLine(char a, char b, char c)
+{
+    if (arr[a - '0'] && arr[b - '0'] && arr[c - '0'])
+        return false;
+
+    if ((a == b && a != c) || (b == c && a != b) || (a == c && a != b))
+    {
+        arr[a - '0'] = true;
+        arr[b - '0'] = true;
+        arr[c - '0'] = true;
+        return true;
+    }
+    
+    return false;
+}
+
+/*
 // 가로
 bool checkH(int num)
 {
     if (arr[inp[num][0] - '0'] &&
         arr[inp[num][1] - '0'] &&
         arr[inp[num][2] - '0'])
-    return false;
+        return false;
 
     if (inp[num][0] == inp[num][1] && inp[num][0] != inp[num][2] ||
         inp[num][1] == inp[num][2] && inp[num][0] != inp[num][1] ||
@@ -35,7 +52,7 @@ bool checkV(int num)
     if (arr[inp[0][num] - '0'] &&
         arr[inp[1][num] - '0'] &&
         arr[inp[2][num] - '0'])
-    return false;
+        return false;
 
     if (inp[0][num] == inp[1][num] && inp[0][num] != inp[2][num] ||
         inp[1][num] == inp[2][num] && inp[0][num] != inp[1][num] ||
@@ -50,10 +67,29 @@ bool checkV(int num)
 
     return false;
 }
+*/
 
 int main() {
     for (int i = 0; i < 3; i++) cin >> inp[i];
 
+    for (int i = 0; i < 3; i++)
+    {
+        if (checkLine(inp[i][0], inp[i][1], inp[i][2]))
+            ans++;
+    }
+
+    for (int j = 0; j < 3; j++)
+    {
+        if (checkLine(inp[0][j], inp[1][j], inp[2][j]))
+            ans++;
+    }
+
+    if (checkLine(inp[0][0], inp[1][1], inp[2][2]))
+        ans++;
+    if (checkLine(inp[2][0], inp[1][1], inp[0][2]))
+        ans++;
+
+    /*
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
@@ -91,6 +127,7 @@ int main() {
             arr[inp[0][2] - '0']))
             ans++;
     }
+    */
 
     cout << ans;
 
