2a7bd5fa3c698916b0430e921367b0efbb0b1231
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-05-17 23:43:39 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250517/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a = 0, b = 0, c = 0;
+
+    cin >> a >> b >> c;
+
+    cout << fixed;
+    cout.precision(3);
+    cout << a << endl;
+    cout << b << endl;
+    cout << c;
+    
+    return 0;
+}
