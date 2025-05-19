6deaf5f4abd9323232875463a2590db10d754236
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-05-19 22:25:57 +0900

    Title: 수들의 최대 차, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250519/수들의 최대 차/maximum-difference-in-numbers.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N, K;
+int arr[1000];
+int ans = -1;
+
+int main() {
+    cin >> N >> K;
+
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + N);
+
+    for (int i = 0; i < N; i++)
+    {
+        int min = arr[i];
+        int cnt = 0;
+
+        if (min == arr[i - 1])
+            continue;
+        
+        for (int j = 0; j < N; j++)
+        {
+            if (arr[j] >= min && arr[j] <= min + K)
+                cnt++;
+        }
+
+        ans = max(ans, cnt);
+    }
+
+    cout << ans;
+
+    return 0;
+}
