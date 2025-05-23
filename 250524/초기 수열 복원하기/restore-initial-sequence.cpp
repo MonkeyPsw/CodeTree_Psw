0d5356cfde5c0bcad833f864333dc6174d3abf24
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-05-24 01:11:29 +0900

    Title: 초기 수열 복원하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250524/초기 수열 복원하기/restore-initial-sequence.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int adjacent[1000];
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n - 1; i++) {
+        cin >> adjacent[i];
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        int oriNum[1001] = {};
+        bool usedNum[1001] = {};
+        bool isDone = true;
+
+        oriNum[0] = i;
+        usedNum[i] = true;
+
+        for (int j = 0; j < n - 1; j++)
+        {
+            int nextOriNum = adjacent[j] - oriNum[j];
+
+            if (usedNum[nextOriNum])
+            {
+                isDone = false;
+                break;
+            }
+
+            oriNum[j + 1] = nextOriNum;
+            usedNum[oriNum[j + 1]] = true;
+        }
+
+        if (isDone)
+        {
+            for (int i = 0; i < n; i++)
+                cout << oriNum[i] << " ";
+            
+            break;
+        }
+    }
+
+    return 0;
+}
