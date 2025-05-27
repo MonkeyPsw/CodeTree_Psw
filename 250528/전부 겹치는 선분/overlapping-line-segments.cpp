bf230703d4d9898d9f2f18a93fa4987d6c3dd637
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-05-28 01:08:18 +0900

    Title: 전부 겹치는 선분, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250528/전부 겹치는 선분/overlapping-line-segments.cpp
+++ 250528/전부 겹치는 선분/overlapping-line-segments.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 int n;
 int x1[100], x2[100];
+int x1Max, x2Min = 101;
 
 int main() {
     cin >> n;
 
     for (int i = 0; i < n; i++) {
         cin >> x1[i] >> x2[i];
+        x1Max = max(x1Max, x1[i]);
+        x2Min = min(x2Min, x2[i]);
     }
 
+    if (x1Max <= x2Min)
+        cout << "Yes";
+    else
+        cout << "No";
+
+    // x1시작점 중 가장 큰 값과 x2끝점 중 가장 작은 값을 비교
+
+    /* 의도에 맞는 풀이가 아니긴 했다
     for (int i = 1; i <= 100; i++)
     {
         bool isOverlap = true;
@@ -33,6 +45,7 @@ int main() {
     }
 
     cout << "No";
+    */
 
     return 0;
 }
