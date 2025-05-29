62219d92255305a71fb0a73d184d2740ee45d293
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-05-30 01:16:20 +0900

    Title: 비둘기와 전기줄, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250530/비둘기와 전기줄/pigeons-and-electric-cords.cpp
+++ 250530/비둘기와 전기줄/pigeons-and-electric-cords.cpp
@@ -7,7 +7,8 @@ int N;
 int pigeon[100];
 int position[100];
 int ans;
-string pigeonPos[11];
+string strPigeonPos[11];
+int pigeonPos[11];
 
 int main() {
     cin >> N;
@@ -15,22 +16,27 @@ int main() {
         cin >> pigeon[i] >> position[i];
     }
 
+    /*
     // 이거도 마음에 들진않네
     for (int i = 0; i < N; i++)
-        pigeonPos[pigeon[i]] += to_string(position[i]);
+        strPigeonPos[pigeon[i]] += to_string(position[i]);
 
     for (int i = 1; i <= 10; i++)
     {
-        if (pigeonPos[i].length() >= 2)
+        if (strPigeonPos[i].length() >= 2)
         {
-            for (int j = 1; j < pigeonPos[i].length(); j++)
+            for (int j = 1; j < strPigeonPos[i].length(); j++)
             {
-                if (pigeonPos[i][j] != pigeonPos[i][j - 1])
+                if (strPigeonPos[i][j] != strPigeonPos[i][j - 1])
                     ans++;
             }
         }
     }
+    */
 
+    // 해설을 보니 이 아이디어가 맞긴했네
+    // 초기위치 처음 보인 값으로 지정하고 위치가 달라지면 증가 후 갱신
+    // 여기서 처음에 -1로 전체 초기화 하고 하려다가 위 코드로 했는데 으앙
     /* 뭔가 해보다가 그냥 다시하기로 함
     for (int i = 0; i < N; i++)
     {
@@ -53,7 +59,25 @@ int main() {
             }
         }
     }
-    */    
+    */
+
+    // 그래 이게 맞지
+    for (int i = 1; i <= 10; i++)
+        pigeonPos[i] = -1;
+
+    for (int i = 0; i < N; i++)
+    {
+        if (pigeonPos[pigeon[i]] < 0)
+            pigeonPos[pigeon[i]] = position[i];
+        else
+        {
+            if (pigeonPos[pigeon[i]] != position[i])
+            {
+                ans++;
+                pigeonPos[pigeon[i]] = position[i];
+            }
+        }
+    }
 
     cout << ans;
 
