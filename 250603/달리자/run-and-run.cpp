528efd446d5260547d6edd12b7e16f993910a126
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-06-03 21:01:53 +0900

    Title: 달리자, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250603/달리자/run-and-run.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int A[100];
+int B[100];
+int ans;
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        cin >> B[i];
+    }
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (A[i] < B[i])
+        {
+            for (int j = i - 1; j >= 0; j--)
+            {
+                while (A[j] > B[j])
+                {
+                    A[j]--;
+                    A[i]++;
+                    ans += i - j;
+
+                    if (A[i] == B[i])
+                        break;
+                }
+            }
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}
