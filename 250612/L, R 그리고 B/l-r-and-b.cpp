327d75dfc961e3f373542e186d8d7b3870829649
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-06-12 22:55:23 +0900

    Title: L, R 그리고 B, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250612/L, R 그리고 B/l-r-and-b.cpp
+++ 250612/L, R 그리고 B/l-r-and-b.cpp
@@ -15,7 +15,8 @@ int main() {
 
     // 이게 왜 여러가지 상황이 있는거지
     // 각 문자가 하나씩이면 그냥 LB가로세로차이합-1 아닌가
-    // 아하 전부 같은 가로줄이나 세로줄인 경우가 있구나
+    // 아하 전부 같은 가로줄이나 세로줄인 경우가 있구나 엄마야
+    // 전부 같은줄인데 L이랑 B가 먼저 가까운 경우도 있구나 세상에
 
     for (int i = 0; i < 10; i++)
     {
@@ -41,11 +42,22 @@ int main() {
         }
     }
 
-    if (length_L == length_B && length_B == length_R ||
-        width_L == width_B && width_B == width_R)
-        cout << abs(length_L - length_B) + abs(width_L - width_B) + 1;
-    else
-        cout << abs(length_L - length_B) + abs(width_L - width_B) - 1;
+    int dist = abs(length_L - length_B) + abs(width_L - width_B) - 1;
+
+    if (width_L == width_B && width_B == width_R)
+    {
+        if (length_R > length_L && length_R < length_B ||
+            length_R < length_L && length_R > length_B)
+            dist += 2;
+    }
+    else if (length_L == length_B && length_B == length_R)
+    {
+        if (width_R > width_L && width_R < width_B ||
+            width_R < width_L && width_R > width_B)
+            dist += 2;
+    }
+    
+    cout << dist;
 
     return 0;
 }
