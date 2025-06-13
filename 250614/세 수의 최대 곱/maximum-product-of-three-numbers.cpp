24acb822db58e3b2743bfbb9a16eeccb3df05e2c
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-06-14 01:17:51 +0900

    Title: 세 수의 최대 곱, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250614/세 수의 최대 곱/maximum-product-of-three-numbers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int arr[100000];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + n);
+
+    int tmp1 = max(arr[0] * arr[1] * arr[2], arr[0] * arr[1] * arr[n - 1]);
+    int tmp2 = max(arr[0] * arr[n - 2] * arr[n - 1], arr[n - 3] * arr[n - 2] * arr[n - 1]);
+
+    cout << max(tmp1, tmp2);
+
+    return 0;
+}
