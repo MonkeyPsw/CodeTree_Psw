c56ec2612d653dc3e0f2521be4529d1c5797c408
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-07-06 17:40:27 +0900

    Title: 기수 정렬 구현, Time: 34ms, Memory: 1MB, Status: Passed - Codetree

---
+++ 250706/기수 정렬 구현/implement-radix-sort.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int n;
+int arr[100000];
+int maxVal, k;
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+
+        maxVal = max(maxVal, arr[i]);
+    }
+
+    while (maxVal > 0)
+    {
+        maxVal /= 10;
+        k++;
+    }
+
+    int digit = 1;
+
+    for (int pos = k - 1; pos >= 0; pos--)
+    {
+        vector<int> arr_digits[10];
+
+        for (int i = 0; i < n; i++)
+            arr_digits[(arr[i] / digit) % 10].push_back(arr[i]);
+        
+
+        int cnt = 0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < arr_digits[i].size(); j++)
+                arr[cnt++] = arr_digits[i][j];
+        }
+
+        digit *= 10;
+    }
+
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
