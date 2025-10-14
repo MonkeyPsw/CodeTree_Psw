e7cfeb339381dcde593253e2cbccd55973cd4734
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-10-14 23:07:53 +0900

    Title: 0 나올때까지 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251014/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+
+    while (true)
+    {
+        cin >> num;
+
+        if (num == 0)
+            break;
+        
+        cout << num << endl;
+    }
+    
+    return 0;
+}
