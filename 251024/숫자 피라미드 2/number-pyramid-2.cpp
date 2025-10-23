37e556c2307d1c6e7c364b7ea6a1b80bd71aa09e
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-10-24 00:39:23 +0900

    Title: 숫자 피라미드 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251024/숫자 피라미드 2/number-pyramid-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, cnt = 1;;
+
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j < i; j++)
+            cout << cnt++ << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
