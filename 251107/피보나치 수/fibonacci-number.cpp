1b3fce86bd5600ccdd4a831f83338f6d18f68557
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-11-07 23:16:08 +0900

    Title: 피보나치 수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251107/피보나치 수/fibonacci-number.cpp
+++ 251107/피보나치 수/fibonacci-number.cpp
@@ -4,10 +4,31 @@ using namespace std;
 
 int N;
 int fibo[46];
+int memo[46];
+
+void InitMemo()
+{
+    for (int i = 1; i <= 46; i++)
+        memo[i] = -1;
+}
+
+int Fibonacci(int n)
+{
+    if (memo[n] != -1)
+        return memo[n];
+
+    if (n <= 2)
+        memo[n] = 1;
+    else
+        memo[n] = Fibonacci(n - 1) + Fibonacci(n - 2);
+
+    return memo[n];
+}
 
 int main() {
     cin >> N;
 
+    /*
     fibo[1] = 1;
     fibo[2] = 1;
 
@@ -15,6 +36,11 @@ int main() {
         fibo[i] = fibo[i - 1] + fibo[i - 2];
 
     cout << fibo[N];
+    */
+
+    InitMemo();
+
+    cout << Fibonacci(N);    
 
     return 0;
 }
