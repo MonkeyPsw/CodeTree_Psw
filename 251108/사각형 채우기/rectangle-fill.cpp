14755be55fa859e2d2c5497bfa8deef3241007ca
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-11-08 17:05:32 +0900

    Title: 사각형 채우기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251108/사각형 채우기/rectangle-fill.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int box[1001];
+
+int main() {
+    cin >> n;
+
+    box[1] = 1;
+    box[2] = 2;
+
+    for (int i = 3; i <= n; i++)
+        box[i] = (box[i - 1] + box[i - 2]) % 10007;
+
+    cout << box[n];
+
+    return 0;
+}
