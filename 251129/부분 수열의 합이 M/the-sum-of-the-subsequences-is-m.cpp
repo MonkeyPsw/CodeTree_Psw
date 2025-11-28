6630fc27a6bc3b586427779603b76ba1882513ae
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-11-29 02:10:57 +0900

    Title: 부분 수열의 합이 M, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251129/부분 수열의 합이 M/the-sum-of-the-subsequences-is-m.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+int n, m;
+int A[100];
+int dp[10001];
+int ans;
+
+void Init()
+{
+    for (int i = 0; i <= m; i++)
+        dp[i] = INT_MAX;
+    
+    dp[0] = 0;
+}
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+
+    Init();
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = m; j >= 0; j--)
+        {
+            if (j >= A[i])
+            {
+                if (dp[j - A[i]] == INT_MAX)
+                    continue;
+                
+                dp[j] = min(dp[j], dp[j - A[i]] + 1);
+            }
+        }
+    }
+
+    cout << (dp[m] == INT_MAX ? -1 : dp[m]);
+
+    return 0;
+}
