d201684e6cce6c7ff75e2bb0b9922ae99df9368a
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2025-12-13 02:52:49 +0900

    Title: 원소의 합이 0, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251213/원소의 합이 0/the-sum-of-the-elements-is-0.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+int n;
+int A[5000];
+int B[5000];
+int C[5000];
+int D[5000];
+unordered_map<long long, long long> ump;
+int ans;
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) cin >> A[i];
+
+    for (int i = 0; i < n; i++) cin >> B[i];
+
+    for (int i = 0; i < n; i++) cin >> C[i];
+
+    for (int i = 0; i < n; i++) cin >> D[i];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            ump[A[i] + B[j]]++;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            int tmp = C[i] + D[j];
+
+            if (ump.find(-tmp) != ump.end())
+                ans++;
+        }
+    }
+    
+    cout << ans;
+
+    return 0;
+}
