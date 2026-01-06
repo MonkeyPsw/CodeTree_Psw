f108109b546228dcafb88cd0cab4af1379b286f8
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2026-01-06 13:59:48 +0900

    Title: 최소 정수 출력, Time: 121ms, Memory: 2MB, Status: Passed - Codetree

---
+++ 260106/최소 정수 출력/min-integer-output.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int n;
+int x[200000];
+priority_queue<int> pq;
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> x[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (x[i])
+            pq.push(-x[i]);
+        else
+        {
+            if (pq.empty())
+                cout << 0 << endl;
+            else
+            {
+                cout << -pq.top() << endl;
+                pq.pop();
+            }
+        }
+    }
+
+    return 0;
+}
