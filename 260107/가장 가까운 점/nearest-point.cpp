9fba0c72ac0ebe90e99c6d48b842a97d212d4273
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2026-01-07 00:25:41 +0900

    Title: 가장 가까운 점, Time: 73ms, Memory: 2MB, Status: Passed - Codetree

---
+++ 260107/가장 가까운 점/nearest-point.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <tuple>
+#include <functional>
+
+using namespace std;
+using T = tuple<int, int, int>;
+
+const int MAX_N = 100000;
+int n, m;
+int x[MAX_N], y[MAX_N];
+//priority_queue<tuple<int, int, int>, vector<tuple<int, int, int> >, greater<tuple<int, int, int> > > pq;
+priority_queue<T, vector<T>, greater<T> > pq;
+int ansX, ansY;
+
+int main() {
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        cin >> x[i] >> y[i];
+
+        pq.push(make_tuple(x[i] + y[i], x[i], y[i]));
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        int nx = 0, ny = 0;
+        tuple<int, int, int> nearPoint = pq.top();
+
+        tie(ignore, nx, ny) = nearPoint;
+        nx += 2;
+        ny += 2;
+
+        pq.push(make_tuple(nx + ny, nx, ny));
+        pq.pop();
+    }
+
+    tuple<int, int, int> nearestPoint = pq.top();
+    tie(ignore, ansX, ansY) = nearestPoint;
+
+    cout << ansX << " " << ansY;
+
+    return 0;
+}
