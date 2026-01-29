cf5a37a1a53c5ee9fcb5b510dfc790801c5daa78
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2026-01-30 03:51:09 +0900

    Title: 구간 크기의 합, Time: 25ms, Memory: 1MB, Status: Passed - Codetree

---
+++ 260130/구간 크기의 합/sum-of-interval-sizes.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <utility>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int a[100000], b[100000];
+pair<int, int> line[200000];
+int sum, ans, startIdx;
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i] >> b[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        line[i] = make_pair(a[i], 1);
+        line[i + n] = make_pair(b[i], -1);
+    }
+    
+    sort(line, line + 2 * n);
+
+    for (int i = 0; i < 2 * n; i++)
+    {
+        if (line[i].second == 1 && sum == 0)
+            startIdx = line[i].first;
+
+        sum += line[i].second;
+
+        if (line[i].second == -1 && sum == 0)
+            ans += line[i].first - startIdx;
+    }
+
+    cout << ans;
+
+    return 0;
+}
