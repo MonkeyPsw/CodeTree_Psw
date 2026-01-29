632a2a51bb204b4f263652fe7dbfc7ae4f96ad07
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2026-01-29 18:28:47 +0900

    Title: 호텔 예약, Time: 94ms, Memory: 2MB, Status: Passed - Codetree

---
+++ 260129/호텔 예약/reserve-hotel.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <utility>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int s[100000];
+int e[100000];
+pair<int, int> line[200000];
+int cnt, ans;
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> s[i] >> e[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        line[i] = make_pair(s[i], 1);
+        line[i + n] = make_pair(e[i] + 1, -1);
+    }
+
+    sort(line, line + 2 * n);
+
+    for (int i = 0; i < 2 * n; i++)
+    {
+        cnt += line[i].second;
+
+        if (ans < cnt)
+            ans = cnt;
+    }
+
+    cout << ans;
+
+    return 0;
+}
