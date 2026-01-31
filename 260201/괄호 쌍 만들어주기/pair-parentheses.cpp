9d08b8c82443b7a62459037b61d168d05b78a0ff
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2026-02-01 05:41:27 +0900

    Title: 괄호 쌍 만들어주기, Time: 11ms, Memory: 1MB, Status: Wrong Answer - Codetree

---
+++ 260201/괄호 쌍 만들어주기/pair-parentheses.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string A;
+int close[100000];
+int ans;
+
+int main() {
+    cin >> A;
+
+    for (int i = A.length() - 2; i >= 0; i--)
+    {
+        close[i] = close[i + 1];
+
+        if (A[i] == ')' && A[i + 1] == ')')
+            close[i]++;
+    }
+
+    for (int i = 1; i < A.length(); i++)
+    {
+        if (A[i] == '(' && A[i - 1] == '(')
+            ans += close[i + 1];
+    }
+
+    cout << ans;
+
+    return 0;
+}
