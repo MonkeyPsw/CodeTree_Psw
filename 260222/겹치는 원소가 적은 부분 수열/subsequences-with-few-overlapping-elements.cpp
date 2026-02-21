d282048b6e6e36b2fcf347c93c72cc8a62943bc8
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2026-02-22 02:11:14 +0900

    Title: 겹치는 원소가 적은 부분 수열, Time: 18ms, Memory: 1MB, Status: Wrong Answer - Codetree

---
+++ 260222/겹치는 원소가 적은 부분 수열/subsequences-with-few-overlapping-elements.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+int n, k;
+int arr[100000];
+unordered_map<int, int> ump;
+int j, ans;
+
+int main() {
+    cin >> n >> k;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        while (j < n && ump[arr[j]] < k)
+        {
+            ump[arr[j]]++;
+            j++;
+        }
+
+        if (j < n && ump[arr[j]] >= k)
+            ans = max(ans, j - i);
+        
+        ump[arr[i]]--;
+    }
+
+    cout << ans;
+
+    return 0;
+}
