d0f128f4051a0e743b2e8cb4c3971e07e8e540c3
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2026-03-01 00:30:28 +0900

    Title: 트리의 부모 노드, Time: 170ms, Memory: 6MB, Status: Passed - Codetree

---
+++ 260301/트리의 부모 노드/parent-node-of-the-tree.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n;
+int from[100000], to[100000];
+vector<int> tree[100001];
+int parent[100001];
+bool visited[100001];
+
+void DFS(int node)
+{
+    visited[node] = true;
+
+    for (int next : tree[node])
+    {
+        if (!visited[next])
+        {
+            parent[next] = node;
+            DFS(next);
+        }
+    }
+}
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n - 1; i++) {
+        cin >> from[i] >> to[i];
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        tree[from[i]].push_back(to[i]);
+        tree[to[i]].push_back(from[i]);
+    }
+
+    DFS(1);
+
+    for (int i = 2; i <= n; i++)
+        cout << parent[i] << endl;
+
+    return 0;
+}
