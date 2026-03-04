679485bd9033af400056e9665fdd76930653477e
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2026-03-04 21:24:17 +0900

    Title: 자연수 n개의 합, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260304/자연수 n개의 합/sum-of-n-natural-numbers.cpp
+++ 260304/자연수 n개의 합/sum-of-n-natural-numbers.cpp
@@ -10,13 +10,13 @@ int main() {
     cin >> s;
 
     long long left = 1;
-    long long right = s;
+    long long right = 2000000000;
 
     while (left <= right)
     {
         long long mid = (left + right) / 2;
 
-        if (mid <= (2 * s) / (mid + 1))
+        if (mid * (mid + 1) / 2 <= s)
         {
             left = mid + 1;
             ans = max(ans, mid);
