977669339e8b8b85cf7b488b5008fa71b812f785
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2026-03-15 04:17:42 +0900

    Title: 수 합치기, Time: 40ms, Memory: 1MB, Status: Passed - Codetree

--- 260315/수 합치기/merge-numbers.cpp
+++ 260315/수 합치기/merge-numbers.cpp
@@ -7,6 +7,39 @@ using namespace std;
 
 int n;
 int arr[100000];
+int ans;
+priority_queue<int, vector<int>, greater<int> > pq;
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+        pq.push(arr[i]);
+    
+    while (pq.size() > 1)
+    {
+        int a = pq.top();
+        pq.pop();
+
+        int b = pq.top();
+        pq.pop();
+
+        ans += a + b;
+
+        pq.push(a + b);
+    }
+
+    cout << ans;
+
+    return 0;
+}
+
+/* 굳이 long long 안써도 흠
+int n;
+int arr[100000];
 long long ans;
 priority_queue<long long, vector<long long>, greater<long long> > pq;
 
@@ -36,3 +69,4 @@ int main() {
 
     return 0;
 }
+*/
