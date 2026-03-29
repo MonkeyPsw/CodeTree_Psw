3f878d516edf01c562d50c19f0581f5d00db2134
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2026-03-30 02:39:33 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260330/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int N;
+int B[50000];
+set<int> A;
+int ans;
+
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> B[i];
+    }
+
+    for (int i = 1; i <= N * 2; i++)
+        A.insert(i);
+    
+    for (int i = 0; i < N; i++)
+        A.erase(B[i]);
+
+    for (int i = 0; i < N; i++)
+    {
+        if (A.upper_bound(B[i]) != A.end())
+        {
+            ans++;
+            A.erase(B[i]);
+        }
+        else
+            A.erase(A.begin());
+    }
+
+    cout << ans;
+
+    return 0;
+}
