fcf3bd68a0ce92ac0c9e619dcda1823b4467cf6f
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2026-04-10 02:02:06 +0900

    Title: 트리 판별, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260410/트리 판별/tree-identification.cpp
+++ 260410/트리 판별/tree-identification.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -6,42 +7,72 @@ int m;
 int from[1000], to[1000];
 int indegree[10001];
 bool exist[10001];
+bool visited[10001];
+vector<int> graph[10001];
 int nodeCount;
 int rootCount;
+int root;
+int visitedCount;
 
-int main() {
-    cin >> m;
-        for (int i = 0; i < m; i++) {
-                cin >> from[i] >> to[i];
-                    }
+void DFS(int node)
+{
+    visited[node] = true;
+        visitedCount++;
 
-                        for (int i = 0; i < m; i++)
-                            {
-                                    indegree[to[i]]++;
-                                            exist[from[i]] = true;
-                                                    exist[to[i]] = true;
-                                                        }
+            for (int i = 0; i < graph[node].size(); i++)
+                {
+                        int next = graph[node][i];
 
-                                                            for (int i = 1; i <= 10000; i++)
-                                                                {
-                                                                        if (!exist[i])
-                                                                                    continue;
+                                if (!visited[next])
+                                            DFS(next);
+                                                }
+                                                }
 
-                                                                                            nodeCount++;
+                                                int main() {
+                                                    cin >> m;
+                                                        for (int i = 0; i < m; i++) {
+                                                                cin >> from[i] >> to[i];
+                                                                    }
 
-                                                                                                    if (indegree[i] == 0)
-                                                                                                                rootCount++;
-                                                                                                                        else if (indegree[i] != 1)
-                                                                                                                                {
-                                                                                                                                            cout << 0;
-                                                                                                                                                        return 0;
-                                                                                                                                                                }
-                                                                                                                                                                    }
+                                                                        for (int i = 0; i < m; i++)
+                                                                            {
+                                                                                    indegree[to[i]]++;
+                                                                                            exist[from[i]] = true;
+                                                                                                    exist[to[i]] = true;
+                                                                                                            graph[from[i]].push_back(to[i]);
+                                                                                                                }
 
-                                                                                                                                                                        if (rootCount == 1 && m == nodeCount - 1)
-                                                                                                                                                                                cout << 1;
-                                                                                                                                                                                    else
-                                                                                                                                                                                            cout << 0;
+                                                                                                                    for (int i = 1; i <= 10000; i++)
+                                                                                                                        {
+                                                                                                                                if (!exist[i])
+                                                                                                                                            continue;
 
-                                                                                                                                                                                                return 0;
-                                                                                                                                                                                                }
+                                                                                                                                                    nodeCount++;
+
+                                                                                                                                                            if (indegree[i] == 0)
+                                                                                                                                                                    {
+                                                                                                                                                                                rootCount++;
+                                                                                                                                                                                            root = i;
+                                                                                                                                                                                                    }
+                                                                                                                                                                                                            else if (indegree[i] != 1)
+                                                                                                                                                                                                                    {
+                                                                                                                                                                                                                                cout << 0;
+                                                                                                                                                                                                                                            return 0;
+                                                                                                                                                                                                                                                    }
+                                                                                                                                                                                                                                                        }
+
+                                                                                                                                                                                                                                                            if (rootCount != 1 || m != nodeCount - 1)
+                                                                                                                                                                                                                                                                {
+                                                                                                                                                                                                                                                                        cout << 0;
+                                                                                                                                                                                                                                                                                return 0;
+                                                                                                                                                                                                                                                                                    }
+
+                                                                                                                                                                                                                                                                                        DFS(root);
+
+                                                                                                                                                                                                                                                                                            if (visitedCount == nodeCount)
+                                                                                                                                                                                                                                                                                                    cout << 1;
+                                                                                                                                                                                                                                                                                                        else
+                                                                                                                                                                                                                                                                                                                cout << 0;
+
+                                                                                                                                                                                                                                                                                                                    return 0;
+                                                                                                                                                                                                                                                                                                                    }
