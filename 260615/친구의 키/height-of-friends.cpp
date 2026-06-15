7f33420ee03798ace6ab6253aebdc61cf0c5e2da
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2026-06-15 20:41:20 +0900

    [Passed] 친구의 키 (56ms, 5MB)

---
+++ 260615/친구의 키/height-of-friends.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int n, m;
+int a[100000], b[100000];
+
+vector<int> graph[100001];
+int indegree[100001];
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+        cin >> a[i] >> b[i];
+
+        graph[a[i]].push_back(b[i]);
+        indegree[b[i]]++;
+    }
+
+    queue<int> q;
+
+    for (int i = 1; i <= n; i++) {
+        if (indegree[i] == 0) {
+            q.push(i);
+        }
+    }
+
+    while (!q.empty()) {
+        int cur = q.front();
+        q.pop();
+
+        cout << cur << " ";
+
+        for (int i = 0; i < graph[cur].size(); i++) {
+            int next = graph[cur][i];
+            indegree[next]--;
+
+            if (indegree[next] == 0) {
+                q.push(next);
+            }
+        }
+    }
+
+    return 0;
+}
