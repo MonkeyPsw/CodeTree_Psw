a6b91b5dd0e2fb2f16b57886b6d278dd22eefb36
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2026-07-11 21:02:05 +0900

    [Passed] 부분 문자열의 위치 구하기 (40ms, 9MB)

--- 260711/부분 문자열의 위치 구하기/find-the-location-of-a-substring.cpp
+++ 260711/부분 문자열의 위치 구하기/find-the-location-of-a-substring.cpp
@@ -1,42 +1,66 @@
 #include <iostream>
 #include <string>
 
+#define MAX_N 500000
+
 using namespace std;
 
 string text;
 string pattern;
 
-int pi[500000];
+int p[2] = {31, 37};
+int mod[2] = {1000000007, 1000000009};
+
+long long p_pow[2][MAX_N + 1];
+
+int ToInt(char c) {
+    return c - 'a' + 1;
+}
 
 int main() {
     cin >> text;
     cin >> pattern;
 
     int n = text.size();
-    int m = pattern.size();
+    int l = pattern.size();
 
-    for (int i = 1, j = 0; i < m; i++) {
-        while (j > 0 && pattern[i] != pattern[j]) {
-            j = pi[j - 1];
-        }
+    for (int k = 0; k < 2; k++) {
+        p_pow[k][0] = 1;
 
-        if (pattern[i] == pattern[j]) {
-            pi[i] = ++j;
+        for (int i = 1; i <= n; i++) {
+            p_pow[k][i] = p_pow[k][i - 1] * p[k] % mod[k];
         }
     }
 
-    for (int i = 0, j = 0; i < n; i++) {
-        while (j > 0 && text[i] != pattern[j]) {
-            j = pi[j - 1];
+    long long pattern_hash[2] = {};
+    long long text_hash[2] = {};
+
+    for (int k = 0; k < 2; k++) {
+        for (int i = 0; i < l; i++) {
+            pattern_hash[k] = (pattern_hash[k] + ToInt(pattern[i]) * p_pow[k][l - 1 - i]) % mod[k];
+            text_hash[k] = (text_hash[k] + ToInt(text[i]) * p_pow[k][l - 1 - i]) % mod[k];
+        }
+    }
+
+    if (pattern_hash[0] == text_hash[0] && pattern_hash[1] == text_hash[1]) {
+        cout << 0;
+        return 0;
+    }
+
+    for (int i = 1; i <= n - l; i++) {
+        for (int k = 0; k < 2; k++) {
+            text_hash[k] = (text_hash[k] * p[k]
+                            - ToInt(text[i - 1]) * p_pow[k][l]
+                            + ToInt(text[i + l - 1])) % mod[k];
+
+            if (text_hash[k] < 0) {
+                text_hash[k] += mod[k];
+            }
         }
 
-        if (text[i] == pattern[j]) {
-            if (j == m - 1) {
-                cout << i - m + 1;
-                return 0;
-            } else {
-                j++;
-            }
+        if (pattern_hash[0] == text_hash[0] && pattern_hash[1] == text_hash[1]) {
+            cout << i;
+            return 0;
         }
     }
 
