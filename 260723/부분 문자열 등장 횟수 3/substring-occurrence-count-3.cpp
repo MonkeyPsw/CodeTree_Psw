36e97c689638de1ba743238ccca78f5a8f6393a8
Seungwon Park <75163308+MonkeyPsw@users.noreply.github.com>
2026-07-23 20:58:50 +0900

    [Passed] 부분 문자열 등장 횟수 3 (2ms, 0MB)

---
+++ 260723/부분 문자열 등장 횟수 3/substring-occurrence-count-3.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string T;
+string P;
+
+int pi[100000];
+
+int main() {
+    cin >> T;
+    cin >> P;
+
+    int n = T.size();
+    int m = P.size();
+
+    for (int i = 1, j = 0; i < m; i++) {
+        while (j > 0 && P[i] != P[j]) {
+            j = pi[j - 1];
+        }
+
+        if (P[i] == P[j]) {
+            pi[i] = ++j;
+        }
+    }
+
+    int answer = 0;
+    int last_end = -1;
+
+    for (int i = 0, j = 0; i < n; i++) {
+        while (j > 0 && T[i] != P[j]) {
+            j = pi[j - 1];
+        }
+
+        if (T[i] == P[j]) {
+            if (j == m - 1) {
+                int start = i - m + 1;
+
+                if (start > last_end) {
+                    answer++;
+                    last_end = i;
+                }
+
+                j = pi[j];
+            } else {
+                j++;
+            }
+        }
+    }
+
+    cout << answer;
+
+    return 0;
+}
